d9ba11ad3a29974535b294a2936919395afb7832
VGreg62 <gregory.viandier@outlook.fr>
2019-12-01 16:43:55 +0100

    Ajout des scripts de creation des tables + correction VIANDIER :)

---
+++ script_bdd/create_Table.sql
@@ -0,0 +1,27 @@
+/*Création de la table Produit*/
+CREATE TABLE TProduit
+(
+	NumProduit int NOT NULL AUTO_INCREMENT,
+	NomProduit varchar(255) NOT NULL,
+	PRIMARY KEY (NumProduit),
+);
+
+/*Création de la table Etat*/
+CREATE TABLE TEtat
+(
+	NumEtat int NOT NULL AUTO_INCREMENT,
+	NomEtat varchar(255) NOT NULL,
+	PRIMARY KEY (NumEtat),
+);
+
+/*Création de la table Commande*/
+CREATE TABLE TCommande
+(
+	NumCommande int NOT NULL AUTO_INCREMENT,
+	NumProduit int NOT NULL,
+	NomMagasin varchar(255) NOT NULL,
+	Etat int NOT NULL,
+	PRIMARY KEY (NumCommande),
+	FOREIGN KEY (NumProduit) REFERENCES TProduit(NumProduit),
+	FOREIGN KEY (Etat) REFERENCES TEtat(NumEtat)
+);
---
+++ script_bdd/insert_Date.sql
@@ -0,0 +1,9 @@
+/*Ajout des différents états*/
+
+INSERT INTO TEtat (NomEtat) 
+VALUES 
+("Commandé"),
+("Pris en cours")
+("En Livraison"),
+("Livré");
+
