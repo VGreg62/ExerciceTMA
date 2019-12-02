c70da510e631bc31889fa6b46844588084e00ae0
VGreg62 <gregory.viandier@outlook.fr>
2019-12-02 17:06:21 +0100

    Ajout de la listbox

--- js/commandeMag.js
+++ js/commandeMag.js
@@ -1,16 +1,25 @@
+$(document).ready(addOption);
+
+function addOption(){
+	var result = new Array("Naruto", "Luffy", "Son Goku", "Saitama");
+	for(var i = 0; i < result.length; i++){
+		console.log(result[i]);
+		$("#listShop").append($('<option>', {
+			value: result[i],
+			text: result[i]
+		}));
+	}
+}
+
 function addCommande(){
-	if($('#nameShop').val() === "" || $('#nameShop').val() === null){
+	if($('#listShop').val() === "" || $('#listShop').val() === null){
 		$('.error').removeClass("hide");
 	}
 	else{
 		$('.error').addClass("hide");
 		for(let i = 0; i < 1000; i++){
-			console.log("Magasin " + $('#nameShop').val() + " Commande : " + i);
+			console.log("Magasin " + $('#listShop').val() + " Commande : " + i);
+			/*Requete Ajax insert commande*/
 		}
 	}
 }
-
-/* TODO
- * Faire la connexion à la bdd
- * Faire la connexion Ajax
- */
