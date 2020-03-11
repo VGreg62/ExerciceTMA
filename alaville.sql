48474f8f16c5f66a59c8e4dd146dde8998547aae
VGreg62 <gregory.viandier@outlook.fr>
2020-03-11 15:15:57 +0100

    Add sql file

---
+++ alaville.sql
@@ -0,0 +1,51 @@
+CREATE TABLE CLIENT(
+	idClient INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nom varchar(255),
+	prenom varchar(255)
+);
+
+CREATE TABLE MAGASIN(
+	idMagasin INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nom varchar(255) 
+);
+
+CREATE TABLE PRODUIT(
+	idProduit INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	nom varchar(255),
+	prix double
+);
+
+CREATE TABLE COMMANDE(
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	idProduit INTEGER,
+	idMagasin INTEGER,
+	idClient INTEGER,
+	dateAchat DATE,
+	quantite INTEGER,
+	FOREIGN KEY (idProduit) REFERENCES PRODUIT(idProduit),
+	FOREIGN KEY (idMagasin) REFERENCES MAGASIN(idMagasin),
+	FOREIGN KEY (idClient) REFERENCES CLIENT(idClient)
+);
+
+INSERT INTO CLIENT(nom, prenom) VALUES ('DEPARDIEU','Gérard');
+INSERT INTO CLIENT(nom, prenom) VALUES ('POUTINE','Vladimir');
+INSERT INTO CLIENT(nom, prenom) VALUES ('RASPOUTINE','Gregory');
+INSERT INTO CLIENT(nom, prenom) VALUES ('BOGDANOFF','Igor');
+INSERT INTO CLIENT(nom, prenom) VALUES ('BOGDANOFF','Grichka');
+
+INSERT INTO MAGASIN(nom) VALUES ('Auchan');
+INSERT INTO MAGASIN(nom) VALUES ('Carrefour');
+INSERT INTO MAGASIN(nom) VALUES ('Leclerc');
+INSERT INTO MAGASIN(nom) VALUES ('Intermarché');
+INSERT INTO MAGASIN(nom) VALUES ('Casino');
+
+INSERT INTO PRODUIT(nom, prix) VALUES ('Vodka',1.1);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Chips',1.2);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Biere',1.3);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Vin',1.4);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Rhum',1.5);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Blini',1.6);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Vatrouchka',1.7);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Chapka',1.8);
+INSERT INTO PRODUIT(nom, prix) VALUES ('K-Way',1.9);
+INSERT INTO PRODUIT(nom, prix) VALUES ('Bortsch',1.0);
